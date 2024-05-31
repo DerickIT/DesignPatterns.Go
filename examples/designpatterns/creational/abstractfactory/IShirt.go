cbc540781c30c1663dba298660f5e02622f3e6fb
Derick <charleszhn@hotmail.com>
2024-06-01 00:21:49 +0800

    补充

--- examples/designpatterns/creational/abstractfactory/IShirt.go
+++ examples/designpatterns/creational/abstractfactory/IShirt.go
@@ -11,3 +11,19 @@ type Shirt struct {
 	logo string
 	size int
 }
+
+func (s *Shirt) setLogo(logo string) {
+	s.logo = logo
+}
+
+func (s *Shirt) getLogo() string {
+	return s.logo
+}
+
+func (s *Shirt) setSize(size int) {
+	s.size = size
+}
+
+func (s *Shirt) getSize() int {
+	return s.size
+}
