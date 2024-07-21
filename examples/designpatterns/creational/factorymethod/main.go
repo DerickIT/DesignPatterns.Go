4e14d2409de81488737574288b1d1ebfa82a17a7
Derick <charleszhn@hotmail.com>
2024-07-22 00:10:21 +0800

    更新新状态

--- examples/designpatterns/creational/factorymethod/main.go
+++ examples/designpatterns/creational/factorymethod/main.go
@@ -34,7 +34,34 @@ func (mf *MotorcycleFactory) CreateVehicle() Vehicle {
 	return &Motorcycle{}
 }
 
+type VehicleType string
+
+const (
+	CarType        VehicleType = "car"
+	MotorcycleType VehicleType = "motorcycle"
+)
+
+var vehicleFactories = map[VehicleType]func() Vehicle{
+	CarType:        func() Vehicle { return &Car{} },
+	MotorcycleType: func() Vehicle { return &Motorcycle{} },
+}
+
+func GetVehicle(vehicleType VehicleType) Vehicle {
+	if factory, ok := vehicleFactories[vehicleType]; ok {
+		return factory()
+	}
+	return nil
+}
+
 func main() {
+
+	car1 := GetVehicle(CarType)
+	fmt.Println(car1.Drive())
+	motorcycle1 := GetVehicle(MotorcycleType)
+	fmt.Println(motorcycle1.Drive())
+
+	fmt.Println("------------------------")
+
 	carFactory := &CarFactory{}
 	car := carFactory.CreateVehicle()
 	fmt.Println(car.Drive())
