40471c85c860d41e63eee3a0d15399271456e082
Derick <charleszhn@hotmail.com>
2024-08-05 21:35:41 +0800

    add blockchain magic

---
+++ examples/blockchain/easyblockchain/blockchain.go
@@ -0,0 +1,16 @@
+package easyblockchain
+
+type Blockchain struct {
+	blocks []*Block
+}
+
+func (bc *Blockchain) AddBlock(data string) {
+	pewvBlock := bc.blocks[len(bc.blocks)-1]
+	newBlock := NewBlock(data, pewvBlock.Hash)
+	bc.blocks = append(bc.blocks, newBlock)
+
+}
+
+func NewBlockchain() *Blockchain {
+	return &Blockchain{[]*Block{NewGensisBlock()}}
+}
