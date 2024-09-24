205299b3ee7421a23b02d1ad9da77eeaf3e5dda0
Derick <charleszhn@hotmail.com>
2024-09-24 18:05:07 +0800

    加锁+cond

--- examples/goroutine-demo/testcond/main.go
+++ examples/goroutine-demo/testcond/main.go
@@ -7,26 +7,36 @@ import (
 )
 
 func race() {
+
 	cond := sync.NewCond(&sync.Mutex{})
 	var wg sync.WaitGroup
+	ready := false
 	wg.Add(11)
 	for i := 0; i < 10; i++ {
+		// wg.Add(1)
 		go func(num int) {
 			defer wg.Done()
-			fmt.Println(num, "waiting")
+			fmt.Println("wait", num)
 			cond.L.Lock()
-			cond.Wait()
-			fmt.Println(num, "is run ....")
+			for !ready {
+				cond.Wait()
+			}
+			fmt.Println("go", num)
 			cond.L.Unlock()
 		}(i)
 	}
-
-	time.Sleep(3 * time.Second)
+	time.Sleep(1 * time.Second)
+	// wg.Add(1)
 	go func() {
 		defer wg.Done()
-		fmt.Println("broadcast 裁判就wei")
-		fmt.Println("broadcast ready ....")
+		fmt.Println("裁判员准备")
+
+		cond.L.Lock()
+		ready = true
+
 		cond.Broadcast()
+		cond.L.Unlock()
+		fmt.Println("开始比赛")
 	}()
 
 	wg.Wait()
